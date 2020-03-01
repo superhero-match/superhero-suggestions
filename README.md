0eb69aa69b58cdab5e25f6346b46b97632b00fda
Nikolajus Karpovas <mwsoftofficial@gmail.com>
2020-03-01 17:33:21 +0100

    Opensource all the things

---
+++ README.md
@@ -0,0 +1,13 @@
+## Superhero Match
+Superhero Match is an open source dating app.
+
+## superhero-suggestions
+This micro-service is responsible for providing suggestions to users. 
+
+## Becoming Contributor
+If you want to become contributor, please send email to this address - `superheromatch@gmail.com`.
+
+## License
+Copyright 2019-2020 MWSOFT
+
+Licensed under the GPLv3: http://www.gnu.org/licenses/gpl-3.0.html
--- internal/config/cache.go
+++ internal/config/cache.go
@@ -1,8 +1,21 @@
+/*
+  Copyright (C) 2019 - 2020 MWSOFT
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package config
 
 // Cache holds all the configuration settings for the Redis client.
 type Cache struct {
-	Address                string `env:"REDIS_ADDRESS" default:"192.168.178.17"`
+	Address                string `env:"REDIS_ADDRESS" default:"localhost"`
 	Port                   string `env:"REDIS_PORT" default:":6379"`
 	Password               string `env:"REDIS_PASSWORD" default:"Awesome85**"`
 	DB                     int    `env:"REDIS_DB" default:"0"`
